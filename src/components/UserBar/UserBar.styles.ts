7e0deed0547bfd7838cbca846a4f6048d41c081b
eduard-ops <epustynnik@gmail.com>
2022-10-03 02:03:51 +0300

    comm

--- src/components/UserBar/UserBar.styles.ts
+++ src/components/UserBar/UserBar.styles.ts
@@ -29,5 +29,10 @@ export const UsersList = styled.ul`
     border-radius: 8px;
     cursor: pointer;
     margin-bottom: 16px;
+    &:hover {
+      box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset,
+        rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+      color: white;
+    }
   }
 `;
--- src/components/UserBar/UserBar.tsx
+++ src/components/UserBar/UserBar.tsx
@@ -2,18 +2,36 @@ import { UserContainer, NavTitle, UsersList } from "./UserBar.styles";
 
 import { UsersProps } from "../../types/types";
 
+import { useState } from "react";
+
 const UserBar = ({ name, setReceiverId, setUserSocket, users }: UsersProps) => {
+  const [currentSelected, setCurrentSelected] = useState(undefined);
+
   const filterUser = () => {
     return users.filter((item) => item.name !== name);
+  };
+
+  const changeCurrentSelected = (index: any) => {
+    setCurrentSelected(index);
   };
   return (
     <UserContainer>
       <NavTitle>Online ({filterUser().length}) :</NavTitle>
       <UsersList>
-        {filterUser().map((item) => (
+        {filterUser().map((item, index) => (
           <li
             key={item.id}
+            style={
+              index === currentSelected
+                ? {
+                    boxShadow:
+                      "rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset",
+                    color: "white",
+                  }
+                : { textDecoration: "none" }
+            }
             onClick={() => {
+              changeCurrentSelected(index);
               setUserSocket(item.soketId);
               setReceiverId(item.id);
             }}
